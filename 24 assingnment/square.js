207d00fd43574aeb6ea0c38995b85149a9de6207
hraj55654 <94531887+hraj55654@users.noreply.github.com>
2022-01-02 10:11:24 +0530

    Add files via upload

---
+++ 24 assingnment/square.js
@@ -0,0 +1,9 @@
+var Num = prompt("Enter the number");
+Num = parseInt(Num);
+for(var i=1; i<=Num; i++){
+if(Num == i*i){
+console.log("Print perfect square");
+}/* else{
+console.log("Not a Perfect Square");
+} */
+}
